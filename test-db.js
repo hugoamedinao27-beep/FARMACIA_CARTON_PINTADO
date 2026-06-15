d0d44f6aa6643ca9d1400348eb5651027d736409
hugoamedinao27-beep <hugoamedinao27@gmail.com>
2026-06-15 10:43:09 -0400

    chore: remove test file

--- test-db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-  logging: false
-});
-sequelize.authenticate()
-  .then(() => { console.log('CONEXION OK'); process.exit(0); })
-  .catch(e => { console.log('ERROR:', e.message); process.exit(1); });
